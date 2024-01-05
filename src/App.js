5abcce357ef6b9750705ded568ce0875a37e170e
Orcohen33 <orcohen3322@gmail.com>
2024-01-05 13:41:25 +0200

    update homepage

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>CI/CD.</p>
+        <p>CI/CD. test</p>
       </header>
     </div>
   );
